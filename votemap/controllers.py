59a3a4c734f66b7c4d06f2a3fd6bb7b88670316e
Aidan Skinner <aidan@skinner.me.uk>
2012-05-13 00:14:01 +0100

    controllers: get it basically working

--- votemap/controllers.py
+++ votemap/controllers.py
@@ -1,14 +1,35 @@
 # Copyright 2012 Aidan Skinner <aidan@skinner.me.uk>, all rights
 # reserved
 
-from flask import Blueprint, flash, render_template, redirect, request, url_for
-from mongoengine import Q
+import urllib
 
-from votemap.model import Candidate, PollingStation
+from flask import Blueprint,render_template
+
+from votemap.model import PollingStation
 
 controllers = Blueprint("controllers", __name__,
                         static_folder="static")
 
 @controllers.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    data = []
+    polling_stations = PollingStation.objects.all()
+    for ps in polling_stations:
+        for coords in ps.ne_coords, ps.sw_coords:
+            a, b = coords
+            data.append('%s %s %d'% (a, b, 25 + 20 * len(data)))
+
+    print '\n'.join(data)
+    params = urllib.urlencode({'thedata': '\n'.join(data),
+                               'xresInput':20,
+                               'yresInput':20,
+                               'lowercutoffInput':0.0,
+                               'blendInput':7,
+                               'xsizeInput':800,
+                               'ysizeInput':600,
+                               'zoomInput':14
+                               })
+    f = urllib.urlopen('http://diffent.com/map/mapit10.php',
+                       params)
+        
+    return f.read()
