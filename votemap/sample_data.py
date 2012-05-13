334c1463749f991a0ba0ad6a40b6cab453677dd5
Aidan Skinner <aidan@skinner.me.uk>
2012-05-13 17:18:03 +0100

    sample_data: add Aileen's votes for boxes 236 and 240

--- votemap/sample_data.py
+++ votemap/sample_data.py
@@ -6,7 +6,10 @@ from votemap.model import Box, Candidate, PollingStation, Tally
 def make_sample_data():
     michael = Candidate(name="Michael Shanks", party="Labour")
     michael.save()
+    aileen = Candidate(name="Aileen Colleran", party="Labour")
+    aileen.save()
 
+    
     jordanhill_church = PollingStation(name="Jordan Hill Church",
                                        postcode="G13 1QT",
                                        ne_coords=(55.88564250,
@@ -14,11 +17,16 @@ def make_sample_data():
                                        sw_coords=(55.88461760,
                                                   -4.33007750),
                                        min_box=236, max_box=239)
-    tally = Tally(candidate=michael)
-    tally.votes = [23,43,19,13,7,3,1,0,0,0,1,0,0]
-    tally.save()
     jordanhill_church.save()
-    box = Box(number=236, pollingstation=jordanhill_church)
+    
+    box = Box(number=236, polling_station=jordanhill_church)
+    tally = Tally(candidate=michael)
+    tally.preferences = [23,43,19,13,7,3,1,0,0,0,1,0,0]
+    tally.save()
+    box.votes.append(tally)
+    tally = Tally(candidate=aileen)
+    tally.preferences = [56,37,21,18,4,3,1,0,0,0,0,0,1]
+    tally.save()
     box.votes.append(tally)
     box.save()
 
@@ -31,8 +39,12 @@ def make_sample_data():
                              min_box=240, max_box=241)
     st_tams.save()
     tally = Tally(candidate=michael)
-    tally.votes = [20,56,22,12,5,4,1,0,1,0,0,0,0]
+    tally.preferences = [20,56,22,12,5,4,1,0,1,0,0,0,0]
     tally.save()
-    box = Box(number=240, pollingstation=jordanhill_church)
+    box = Box(number=240, polling_station=st_tams)
+    box.votes.append(tally)
+    tally = Tally(candidate=aileen)
+    tally.preferences = [45,53,28,15,8,1,2,0,0,0,0,0,2]
+    tally.save()
     box.votes.append(tally)
     box.save()
