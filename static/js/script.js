01fbd0bbdad9db2def2db7dd84764fc65637c363
Aidan Skinner <aidan@skinner.me.uk>
2012-05-16 21:10:29 +0100

    add_heat: add a red circle showing the size of the vote at each station

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,12 @@
+$(function() {
+      $.getJSON($SCRIPT_ROOT + '/get_candidate_data', {
+                  candidate_id:  candidate_id
+              }, function(data) {
+                  draw_map(data.viewdata, data.results);
+              });
+      
+});
+
 function draw_map(viewdata, results) {
     var myOptions = {
         center: new google.maps.LatLng(viewdata.centre_lat, viewdata.centre_lon),
@@ -6,15 +15,23 @@ function draw_map(viewdata, results) {
     };
     var map = new google.maps.Map(document.getElementById("map_canvas"),
                                   myOptions);
-
+    for (i in results) {
+        add_heat(results[i], map);
+    }
 }
 
-$(function() {
-      $.getJSON($SCRIPT_ROOT + '/get_candidate_data', {
-                  candidate_id:  candidate_id
-              }, function(data) {
-                  draw_map(data.viewdata, data.result);
-              });
-      
-});
-
+function add_heat(result, map) {
+    // Construct the circle. 
+    var populationOptions = {
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      map: map,
+      center: new google.maps.LatLng(result.lat,
+                                     result.lon),
+      radius: result.total
+    };
+    cityCircle = new google.maps.Circle(populationOptions);
+}
